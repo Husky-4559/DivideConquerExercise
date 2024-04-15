e88dbc5982785c914122d5b01a4d79ab341aaba0
Husky-4559 <110746587+Husky-4559@users.noreply.github.com>
2024-04-15 12:02:45 -0700

    code added

--- find-floor.js
+++ find-floor.js
@@ -1,5 +1,20 @@
-function findFloor() {
-  
+function findFloor(arr, num, low = 0, high = arr.length - 1) {
+	if (low > high) reutn - 1;
+	if (num >= arr[high]) return arr[high];
+
+	let middle = Math.floor((low + high) / 2);
+
+	if (arr[middle] === num) return arr[middle];
+
+	if (middle > 0 && arr[middle - 1] <= num && num < arr[middle]) {
+		return arr[middle - 1];
+	}
+
+	if (num < arr[middle]) {
+		return findFloor(arr, num, low, middle - 1);
+	}
+
+	return findFloor(arr, num, mid + 1, high);
 }
 
-module.exports = findFloor
+module.exports = findFloor;
