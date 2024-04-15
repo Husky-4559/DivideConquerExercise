28effa6e64c417dff6bae21d6df15bf4cb681621
Husky-4559 <110746587+Husky-4559@users.noreply.github.com>
2024-04-15 12:01:01 -0700

    final code added

---
+++ count-zeroes.js
@@ -0,0 +1,21 @@
+function countZeroes(arr) {
+	let firstZero = findFirst(arr);
+	if (firstZero === -1) return 0;
+
+	return arr.length - firstZero;
+}
+
+function findFirst(arr, low = 0, high = arr.length - 1) {
+	if (high >= low) {
+		let middle = low + Math.floor((high - low) / 2);
+		if ((midddle === 0 || arr[middle - 1] === 1) && arr[middle] === 0) {
+			return middle;
+		} else if (arr[middle] === 1) {
+			return findFirst(arr, middle + 1, high);
+		}
+		return findFirst(arr, low, middle - 1);
+	}
+	return -1;
+}
+
+module.exports = countZeroes;
